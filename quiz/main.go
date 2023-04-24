ea676f963ff0e47080c5af6cd7d9c8ef6e5e16c4
Mohamed-Eid <medoeid50@gmail.com>
2023-04-24 10:03:04 +0200

    implementing flags and finishing the challenge

--- quiz/main.go
+++ quiz/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
+	"math/rand"
 	"os"
+	"strings"
+	"time"
 )
 
 type Problem struct {
@@ -23,7 +27,21 @@ func createQuizList(data [][]string) []Problem {
 }
 
 func main() {
-	file, err := os.Open("problems.csv")
+	var (
+		filepath string
+		duration time.Duration
+		shuffle  bool
+	)
+
+	flag.StringVar(&filepath, "path", "problems.csv", "file path")
+	flag.DurationVar(&duration, "duration", 30*time.Second, "quiz timer")
+	flag.BoolVar(&shuffle, "shuffle", false, "shuffle the questions")
+
+	flag.Parse()
+
+	fmt.Printf("exam : %s , duration : %+v , shuffle %v\n", filepath, duration, shuffle)
+
+	file, err := os.Open(filepath)
 	if err != nil {
 		fmt.Printf("Failed to open file : %v\n", err)
 	}
@@ -32,11 +50,27 @@ func main() {
 	r := csv.NewReader(file)
 
 	problems, err := r.ReadAll()
-	correctAnswers := 0
 	if err != nil {
 		fmt.Printf("Failed to reading file : %v\n", err)
 	}
 	quiz := createQuizList(problems)
+	correctAnswers := 0
+	totalQuestions := len(problems)
+
+	timer := time.NewTimer(duration)
+
+	go func() {
+		<-timer.C
+		fmt.Println("Time Expired")
+		fmt.Printf("Your Answer : %d / %d \n", correctAnswers, totalQuestions)
+		os.Exit(0)
+	}()
+
+	if shuffle {
+		rand.Shuffle(len(quiz), func(i, j int) {
+			quiz[i], quiz[j] = quiz[j], quiz[i]
+		})
+	}
 
 	for i, problem := range quiz {
 		fmt.Printf("%d. %s?\n", i+1, problem.Question)
@@ -45,11 +79,17 @@ func main() {
 		if err != nil {
 			fmt.Printf("Failed to reading user input : %v\n", err)
 		}
-		if userAnswer == problem.Answer {
+		if strings.TrimSpace(userAnswer) == problem.Answer {
 			correctAnswers++
 		}
 	}
 
-	fmt.Printf("Your Answer : %d / %d \n", correctAnswers, len(problems))
+	// Stop the timer before it expires
+	if !timer.Stop() {
+		// Drain the channel to prevent a goroutine leak
+		<-timer.C
+	}
+
+	fmt.Printf("Your Answer : %d / %d \n", correctAnswers, totalQuestions)
 
 }
